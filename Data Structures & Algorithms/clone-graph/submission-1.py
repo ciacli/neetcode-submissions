9ec5aa013f56106118a430a95b7293783176f7f8
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-20 13:37:27 +0300

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        d = {}
+        if not node:
+            return None
+        def dfs(root):
+            if not root:
+                return None
+            d.setdefault(root.val, Node(val = root.val, neighbors = []))
+            copy = d[root.val]
+            for neighbor in root.neighbors:
+                neighbor_copy = d[neighbor.val] if neighbor.val in d else dfs(neighbor)
+                if neighbor_copy:
+                    copy.neighbors.append(neighbor_copy)
+            return copy
+        dfs(node)
+        return d[node.val]
+                
