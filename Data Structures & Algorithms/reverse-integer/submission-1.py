efe029e8247b91246d5ecf5e4008eab013df4525
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-20 17:28:25 +0300

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        ans = 0
+        MAX = 2 ** 31 - 1
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        while x:
+            digit = x % 10
+            x = x // 10
+             
+            if (MAX - ans * 10) < digit:
+                return 0
+            ans = ans * 10 + digit
+        return ans * sign
+        
+            
