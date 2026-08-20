1082f1f1180bd61c89b1bce675fbf129eff7e670
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-20 14:26:00 +0300

    Add: islands-and-treasure - submission-5

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-5.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        INF = 2**31 - 1
+        n = len(grid)
+        m = len(grid[0])
+        queue = deque()
+        def isIn(i, j):
+            return i >= 0 and i < n and j >= 0 and j < m
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 0:
+                    grid[i][j] = INF
+                    queue.append((i, j, 0))
+        while queue:
+            i, j, dist = queue.popleft()
+            if not isIn(i, j) or grid[i][j] != INF:
+                continue
+            
+            grid[i][j] = dist
+            dist += 1
+            queue.append((i + 1, j, dist))
+            queue.append((i, j + 1, dist))
+            queue.append((i - 1, j, dist))
+            queue.append((i, j - 1, dist))
