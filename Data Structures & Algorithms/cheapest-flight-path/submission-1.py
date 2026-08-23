a310d972501f74a0ea418530bf9325c22f4d59d9
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-23 15:57:26 +0300

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        INF = float('inf')
+        neighbors = [[] for _ in range(n)]
+        for s, d, p in flights:
+            neighbors[s].append([d, p])
+        d = [INF] * n
+        d[src] = 0
+        tmp = d[:]
+        for _ in range(k + 1):
+            for node in range(n):
+                for neighbor, p in neighbors[node]:
+                    if tmp[neighbor] > d[node] + p:
+                        tmp[neighbor] = d[node] + p
+            d = tmp[:]
+        return d[dst] if d[dst] < INF else -1      
