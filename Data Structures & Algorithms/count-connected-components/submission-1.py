a38a7f4f2c0fe61ff4412f86cc469944fe21fb43
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-24 18:33:25 +0300

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = [0] * n
+        rank = [0] * n
+        ans = n
+        def find(node):
+            while node != parent[node]:
+                parent[node] = parent[parent[node]]
+                node = parent[node]
+            return node
+        for i in range(n):
+            parent[i] = i
+        for ai, bi in edges:
+            ra, rb = find(ai), find(bi)
+            if ra == rb: continue
+            ans -= 1
+            if rank[ra] > rank[rb]:
+                parent[rb] = ra
+            else:
+                parent[ra] = rb
+                if rank[ra] == rank[rb]:
+                    rank[rb] += 1
+        return ans
