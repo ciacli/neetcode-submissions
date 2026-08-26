6ed1cf85465850207021591707a422fa055ab2f1
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-26 17:43:27 +0300

    Add: string-compression - submission-3

---
+++ Data Structures & Algorithms/string-compression/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        last = chars[0]
+        n = len(chars)
+        aux = []
+        cnt = 1
+        i = 1
+        k = 0
+        while i < n:
+            
+            if chars[i] == last:
+                cnt += 1
+            else:
+                if k < n:
+                    chars[k] = last
+                k += 1
+                last = chars[i]
+                if cnt > 1:
+                    nums = []
+                    while cnt:
+                        nums.append(str(cnt % 10))
+                        cnt = cnt // 10
+                    for num in nums[::-1]:
+                        if k < n:
+                            chars[k] = num
+                        k += 1
+                cnt = 1
+            
+            i += 1
+        
+        if k < n:
+            chars[k] = last
+        k += 1
+        if cnt > 1:
+            nums = []
+            while cnt:
+                nums.append(str(cnt % 10))
+                cnt = cnt // 10
+            for num in nums[::-1]:
+                if k < n:
+                    chars[k] = num
+                k += 1
+        return k
