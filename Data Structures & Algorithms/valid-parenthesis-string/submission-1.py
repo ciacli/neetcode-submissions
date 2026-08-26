4ef75cfc642859d4db5a0422184798ddc3b2944f
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-26 13:39:29 +0300

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        stars = []
+        for i, c in enumerate(s):
+            if c == '(':
+                left.append(i)
+            elif c == ')':
+                if left:
+                    left.pop()
+                elif stars:
+                    stars.pop()
+                else:
+                    return False
+            else:
+                stars.append(i)
+        
+        ans = True
+        while left:
+            if stars:
+                if stars[-1] > left[-1]:
+                    stars.pop()
+                    left.pop()
+                else:
+                    return False
+            else:
+                return False
+
+
+        return True
