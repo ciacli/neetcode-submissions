35162a6953346f5c423b763b3127409edc4107c1
ciacli <149512423+ciacli@users.noreply.github.com>
2026-08-26 15:42:52 +0300

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        intervals = sorted(intervals, key = lambda t: t.start)
+        h = []
+        heapq.heappush(h, intervals[0].end)
+        for t in intervals[1:]:
+            #print(len(h))
+            #print(str(t.start) + ' ' + str(t.end))
+            
+            earliest = h[0]
+            if t.start >= earliest:
+                heapq.heappop(h)
+            heapq.heappush(h, t.end)
+            
+        return len(h)
+            
